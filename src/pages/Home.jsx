a09f705a902b391d2bb32dec34c43170337b53c0
fabio-lasalvia <99scorpione@gmail.com>
2025-08-07 13:45:50 +0200

    Inizializzata struttura per Main Content - Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap";
 import InfoProfiloHome from "../components/InfoProfiloHome";
+import MainHome from "../components/MainHome";
 
 function Home() {
   return (
@@ -9,7 +10,9 @@ function Home() {
           <Col md={2}>
             <InfoProfiloHome />
           </Col>
-          <Col md={7}></Col>
+          <Col md={7}>
+            <MainHome />
+          </Col>
           <Col md={3}></Col>
         </Row>
       </Container>
